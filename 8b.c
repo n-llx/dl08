550ff642c29e412b2080cefa27f3e3c2d724fadc
Nils <s.nilsllx@gmail.com>
2022-01-05 17:44:36 +0100

    Programme fonctionnel

---
+++ 8b.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void){
+  double x = 0;
+  printf("Quelle est la valeur a trouver ? : ");
+  scanf("%lf", &x);
+  printf("Quelle est le nombre d'iteration ? : ");
+  int nombre_iteration = 0;
+  scanf("%d", &nombre_iteration);
+  double a_n = x;
+  double b_n = 0.;
+  
+  for(int i = 0; i < nombre_iteration; i++){
+    if(x >= ((a_n + b_n)/2)*exp((a_n + b_n)/2)){
+      b_n = (a_n + b_n)/2;
+    }else if(x < ((a_n + b_n)/2)*exp((a_n + b_n)/2)){
+      a_n = (a_n + b_n)/2;
+    }
+  }
+  printf("Resultat : %lf \n", a_n);
+}
